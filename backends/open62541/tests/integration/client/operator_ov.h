e0ffabc8ebbccaf7d32c723d3b344488f2b959c7
Martina Felber <martina.felber@hotmail.com>
2020-04-25 20:15:10 +0200

    WIP
    TODO: cleanup sorting
    TODO: finish IsSubType method
    TODO: finish print of all attributes
    
    Implement test client which prints full server addressspace
    Build a reference server with different nodesets, using nodeset-compiler
    Build a test server which uses NodeSetLoader to import the nodesets
    Compare test output

---
+++ backends/open62541/tests/integration/client/operator_ov.h
@@ -0,0 +1,17 @@
+#ifndef _OPERATOR_OV_H
+#define _OPERATOR_OV_H
+
+#include "browse_utils.h"
+#include <iostream>
+#include <open62541/types.h>
+
+std::ostream &operator<<(std::ostream &os, const UA_NodeId &Id);
+std::ostream &operator<<(std::ostream &os, const UA_String &Str);
+std::ostream &operator<<(std::ostream &os, const UA_NodeClass &NodeClass);
+std::ostream &operator<<(std::ostream &os,
+                         const UA_QualifiedName &QualifiedName);
+std::ostream &operator<<(std::ostream &os,
+                         const UA_LocalizedText &LocalizedText);
+std::ostream &operator<<(std::ostream &os, const TReference &Reference);
+
+#endif // _OPERATOR_OV_H
---
+++ backends/open62541/tests/integration/client/utils.h
@@ -0,0 +1,11 @@
+#ifndef _UTILS_H
+#define _UTILS_H
+
+#include <fstream>
+#include <open62541/client_config.h>
+#include <open62541/types.h>
+
+UA_Boolean PrintNode(UA_Client *pClient, const UA_NodeId &Id,
+                     std::ofstream &out);
+
+#endif // _UTILS_H
