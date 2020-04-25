dcdbffb092fba81a940c2b4c3e10ca5af6c7a028
mkonnerth <matkonnerth@gmail.com>
2020-04-25 09:57:13 +0200

    assert on malloc/calloc

--- src/NamespaceList.c
+++ src/NamespaceList.c
@@ -12,9 +12,11 @@ struct NamespaceList
 NamespaceList *NamespaceList_new(addNamespaceCb cb)
 {
     NamespaceList *list = (NamespaceList *)calloc(1, sizeof(NamespaceList));
+    assert(list);
     list->cb = cb;
     list->size = 1;
     list->data = (Namespace *)calloc(1, sizeof(Namespace));
+    assert(list->data);
     list->data[0].name = "http://opcfoundation.org/UA/";
     list->data[0].idx = 0;
     return list;
@@ -34,6 +36,7 @@ Namespace *NamespaceList_newNamespace(NamespaceList *list, void *userContext,
     list->size++;
     list->data =
         (Namespace *)realloc(list->data, sizeof(Namespace) * list->size);
+    assert(list->data);
     list->data[list->size - 1].name = uri;
     list->data[list->size - 1].idx = globalIdx;
     return &list->data[list->size - 1];
