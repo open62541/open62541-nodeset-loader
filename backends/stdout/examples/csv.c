ebab54b46e18a5d40a56e57921099e9a89ac9a87
mkonnerth <matkonnerth@gmail.com>
2020-05-17 07:49:43 +0200

    add csv dumper

---
+++ backends/stdout/examples/csv.c
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ *    Copyright 2019 (c) Matthias Konnerth
+ */
+
+#include "backend.h"
+
+static int namespaceIdx = 0;
+int addNamespace(void *userContext, const char *uri) { return namespaceIdx++; }
+
+void dumpNode(void *userContext, const TNode *node)
+{
+    FILE *f = (FILE *)userContext;
+    fprintf(f, "%d:%s,%s\n", node->id.nsIdx, node->id.id,
+            node->browseName.name);
+}
