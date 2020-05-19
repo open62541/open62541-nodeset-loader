df25ba0f17ccc314f217ca2b6f4ab6f9ee49d50a
mkonnerth <matkonnerth@gmail.com>
2020-05-19 07:32:49 +0200

    add license, refactor parser

--- backends/open62541/include/NodesetLoader/dataTypes.h
+++ backends/open62541/include/NodesetLoader/dataTypes.h
@@ -1,3 +1,10 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ *    Copyright 2020 (c) Matthias Konnerth
+ */
+
 #ifndef __BACKEND_OPEN62541_DATATYPES_H__
 #define __BACKEND_OPEN62541_DATATYPES_H__
 #include <open62541/types.h>
