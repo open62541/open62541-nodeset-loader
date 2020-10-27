451cec68a4e99bb54ae0b86035235eb4fc711e0b
matkonnerth <matkonnerth@gmail.com>
2020-10-27 20:01:28 +0100

    Fix build (#133)
    * set visibility via property, add extension
    
    * set symbol visibility also for win32
    
    * fix loader
    
    * add LOADER_EXPORT

--- include/NodesetLoader/arch.h
+++ include/NodesetLoader/arch.h
@@ -8,9 +8,27 @@
 #ifndef NODESETLOADER_ARCH_H
 #define NODESETLOADER_ARCH_H
 
+/* this is taken from open62541.com */
+#if defined(_WIN32) && defined(UA_DYNAMIC_LINKING)
+#ifdef UA_DYNAMIC_LINKING_EXPORT /* export dll */
+#ifdef __GNUC__
+#define LOADER_EXPORT __attribute__((dllexport))
+#else
+#define LOADER_EXPORT __declspec(dllexport)
+#endif
+#else /* import dll */
+#ifdef __GNUC__
+#define LOADER_EXPORT __attribute__((dllimport))
+#else
+#define LOADER_EXPORT __declspec(dllimport)
+#endif
+#endif
+#else /* non win32 */
 #if __GNUC__ || __clang__
 #define LOADER_EXPORT __attribute__((visibility("default")))
 #endif
+#endif
+
 #ifndef LOADER_EXPORT
 #define LOADER_EXPORT /* fallback to default */
 #endif
