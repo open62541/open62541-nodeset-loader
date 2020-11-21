42ffd45fc16aceb3472b1552db8731f80ea88fae
mkonnerth <matkonnerth@gmail.com>
2020-11-21 11:45:58 +0100

    coverage via cmake

--- runLcov.sh
+++ runLcov.sh
@@ -5,5 +5,5 @@ lcov --remove coverage.info '/usr/*' --output-file coverage.info
 lcov --remove coverage.info '*tests*' --output-file coverage.info
 lcov --remove coverage.info '*conan*' --output-file coverage.info
 lcov --list coverage.info
-genhtml coverage.info --output-directory coverage
+genhtml coverage.info --output-directory coverageHtml
 chromium ./coverage/index.html
