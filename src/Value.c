cd9d494298fb6e3787a44cdfeca91e5856314653
mkonnerth <matkonnerth@gmail.com>
2021-05-21 19:46:01 +0200

    add comment

--- src/Value.c
+++ src/Value.c
@@ -139,6 +139,7 @@ static const char* isOnlyWhitespace(const char* value)
     }
     for(const char* c=value;*c!='\0'; c++)
     {
+        // http://www.cplusplus.com/reference/cctype/isspace/
         if(!isspace((unsigned char)*c))
         {
             return value;
