93acfba649e8f5d8485dab0d166208ff45cf05b8
Julius Pfrommer <julius.pfrommer@web.de>
2025-09-10 15:09:01 +0200

    Update FindCheck.cmake

--- cmake/FindCheck.cmake
+++ cmake/FindCheck.cmake
@@ -2,7 +2,7 @@
 #  Once done this will define
 #
 #  CHECK_FOUND - system has check
-#  CHECK_INCLUDE_DIR - the check include directory
+#  CHECK_INCLUDE_DIRS - the check include directory
 #  CHECK_LIBRARIES - check library
 #
 #  This configuration file for finding libcheck is originally from
@@ -16,41 +16,67 @@
 #  BSD license.
 #  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-
-INCLUDE( FindPkgConfig )
-
-# Take care about check.pc settings
-PKG_SEARCH_MODULE( CHECK check )
+if(WIN32)
+  # Manually define CHECK_INSTALL_DIR if vcpkg is not used
+  if(DEFINED VCPKG_INSTALLED_DIR)
+    set(CHECK_INSTALL_DIR "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}")
+    # find_package(check CONFIG REQUIRED)
+  else()
+    if(NOT CHECK_INSTALL_DIR)
+      if("${CMAKE_SIZEOF_VOID_P}" STREQUAL "8")
+        file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}" _programfiles)
+      else()
+        set(_progfiles_x86 "ProgramFiles(x86)")
+        if(NOT "$ENV{${_progfiles_x86}}" STREQUAL "")
+          # under windows 64 bit machine
+          file(TO_CMAKE_PATH "$ENV{${_progfiles_x86}}" _programfiles)
+        else()
+          # under windows 32 bit machine
+          file(TO_CMAKE_PATH "$ENV{ProgramFiles}" _programfiles)
+        endif()
+        unset(_progfiles_x86)
+      endif()
+      if(EXISTS ${_programfiles}/Check/lib/check.lib)
+        MESSAGE(STATUS "${_programfiles}/Check/lib/check.lib exists")
+        set(CHECK_INSTALL_DIR "${_programfiles}/Check")
+      endif()
+      unset (_programfiles)
+    endif()
+  endif()
+else()
+    # Take care about check.pc settings
+    find_package(PkgConfig REQUIRED)
+    PKG_SEARCH_MODULE( CHECK check )
+endif()
 
 # Look for CHECK include dir and libraries
 IF( NOT CHECK_FOUND )
-	IF ( CHECK_INSTALL_DIR )
-		MESSAGE ( STATUS "Using override CHECK_INSTALL_DIR to find check" )
-		SET ( CHECK_INCLUDE_DIR  "${CHECK_INSTALL_DIR}/include" )
-		SET ( CHECK_INCLUDE_DIRS "${CHECK_INCLUDE_DIR}" )
-		FIND_LIBRARY( CHECK_LIBRARY NAMES check PATHS "${CHECK_INSTALL_DIR}/lib" )
-		FIND_LIBRARY( COMPAT_LIBRARY NAMES compat PATHS "${CHECK_INSTALL_DIR}/lib" )
-		SET ( CHECK_LIBRARIES "${CHECK_LIBRARY}" "${COMPAT_LIBRARY}" )
-	ELSE ( CHECK_INSTALL_DIR )
-		FIND_PATH( CHECK_INCLUDE_DIR check.h )
-		FIND_LIBRARY( CHECK_LIBRARIES NAMES check )
-	ENDIF ( CHECK_INSTALL_DIR )
+    IF ( CHECK_INSTALL_DIR )
+      MESSAGE ( STATUS "Using override CHECK_INSTALL_DIR to find check: ${CHECK_INSTALL_DIR}" )
+      SET ( CHECK_INCLUDE_DIRS "${CHECK_INSTALL_DIR}/include" )
+#     Find_library() always returns the 64-bit version in "C:/Program Files/Check/lib" no matter what path is passed in
+#     FIND_LIBRARY( CHECK_LIBRARIES NAMES check PATHS "${CHECK_INSTALL_DIR}/lib" NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH )
+      SET ( CHECK_LIBRARIES "${CHECK_INSTALL_DIR}/lib/check.lib" )
+    ELSE ( CHECK_INSTALL_DIR )
+      FIND_PATH( CHECK_INCLUDE_DIRS check.h )
+      FIND_LIBRARY( CHECK_LIBRARIES NAMES check )
+    ENDIF ( CHECK_INSTALL_DIR )
 
-	IF ( CHECK_INCLUDE_DIR AND CHECK_LIBRARIES )
-		SET( CHECK_FOUND 1 )
-		IF ( NOT Check_FIND_QUIETLY )
-			MESSAGE ( STATUS "Found CHECK: ${CHECK_LIBRARIES}" )
-		ENDIF ( NOT Check_FIND_QUIETLY )
-	ELSE ( CHECK_INCLUDE_DIR AND CHECK_LIBRARIES )
-		IF ( Check_FIND_REQUIRED )
-			MESSAGE( FATAL_ERROR "Could NOT find CHECK" )
-		ELSE ( Check_FIND_REQUIRED )
-			IF ( NOT Check_FIND_QUIETLY )
-				MESSAGE( STATUS "Could NOT find CHECK" )	
-			ENDIF ( NOT Check_FIND_QUIETLY )
-		ENDIF ( Check_FIND_REQUIRED )
-	ENDIF ( CHECK_INCLUDE_DIR AND CHECK_LIBRARIES )
+    IF ( CHECK_INCLUDE_DIRS AND CHECK_LIBRARIES )
+      SET( CHECK_FOUND 1 )
+      IF ( NOT Check_FIND_QUIETLY )
+        MESSAGE ( STATUS "Found CHECK: ${CHECK_LIBRARIES}" )
+      ENDIF ( NOT Check_FIND_QUIETLY )
+    ELSE ( CHECK_INCLUDE_DIRS AND CHECK_LIBRARIES )
+      IF ( Check_FIND_REQUIRED )
+        MESSAGE( FATAL_ERROR "Could NOT find CHECK" )
+      ELSE ( Check_FIND_REQUIRED )
+        IF ( NOT Check_FIND_QUIETLY )
+          MESSAGE( STATUS "Could NOT find CHECK" )
+        ENDIF ( NOT Check_FIND_QUIETLY )
+      ENDIF ( Check_FIND_REQUIRED )
+    ENDIF ( CHECK_INCLUDE_DIRS AND CHECK_LIBRARIES )
 ENDIF( NOT CHECK_FOUND )
 
 # Hide advanced variables from CMake GUIs
-MARK_AS_ADVANCED( CHECK_INCLUDE_DIR CHECK_LIBRARIES )
+MARK_AS_ADVANCED( CHECK_INCLUDE_DIRS CHECK_LIBRARY_DIRS CHECK_LIBRARIES )
