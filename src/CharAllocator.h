28cd7280078bd6c9703e068c5a3bb30923957bd6
Julius Pfrommer <julius.pfrommer@o6-automation.com>
2026-09-03 09:35:56 +0200

    feat(xml): replace libxml2 with yxml

--- src/CharAllocator.h
+++ src/CharAllocator.h
@@ -14,7 +14,6 @@ typedef struct CharArenaAllocator CharArenaAllocator;
 
 CharArenaAllocator *CharArenaAllocator_new(size_t initialSize);
 char *CharArenaAllocator_malloc(struct CharArenaAllocator *arena, size_t size);
-char *CharArenaAllocator_realloc(struct CharArenaAllocator *arena, size_t size);
 void CharArenaAllocator_delete(struct CharArenaAllocator *arena);
 
 #endif
